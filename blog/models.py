b5e877131e5ffc12e32b22de32ad73087d5722ff
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-07-23 00:01:37 +0300

    added tag , author and image fileds to post model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
@@ -7,7 +9,12 @@ class Post(models.Model):
     content = models.TextField(max_length=1500)
     created_date = models.DateTimeField(default=timezone.now())
     draft = models.BooleanField(default=True)
+    author  = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,blank = True,related_name='post_user')
+    tags = TaggableManager()
+    image = models.ImageField(upload_to="post") 
 
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     pass
