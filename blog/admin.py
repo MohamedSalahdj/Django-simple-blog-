f1c33e292ee65fcc5465a86e2387e7eeac6f6e9a
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-12 01:13:36 +0300

    added PostCustomise & CommentCustomise classes in admin.py to customise admin panal

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,12 @@ from .models import Post, Comment
 
 class PostCustmise(SummernoteModelAdmin):
     summernote_fields = '__all__'
+    list_display = ['title','draft','author']
+    list_filter = ['draft','author']
+    search_fields = ['title']
 
+class CommentCustmise(admin.ModelAdmin):
+    list_display = ['post','user']
 
 admin.site.register(Post,PostCustmise)
-admin.site.register(Comment)
+admin.site.register(Comment,CommentCustmise)
