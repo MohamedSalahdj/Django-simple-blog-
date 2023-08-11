fcc7779260d0b8d1c78f4d1f462fda9c1c86fc74
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-12 01:17:12 +0300

    looped in tages in post details page

--- blog/admin.py
+++ blog/admin.py
@@ -3,14 +3,14 @@ from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
 
 
-class PostCustmise(SummernoteModelAdmin):
+class PostCustomise(SummernoteModelAdmin):
     summernote_fields = '__all__'
     list_display = ['title','draft','author']
     list_filter = ['draft','author']
     search_fields = ['title']
 
-class CommentCustmise(admin.ModelAdmin):
+class CommentCustomise(admin.ModelAdmin):
     list_display = ['post','user']
 
-admin.site.register(Post,PostCustmise)
-admin.site.register(Comment,CommentCustmise)
+admin.site.register(Post,PostCustomise)
+admin.site.register(Comment,CommentCustomise)
