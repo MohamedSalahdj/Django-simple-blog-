097d0597249af117ff6a702be5ced58cf1d20630
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-12 18:29:00 +0300

    modified edit_post page

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,16 @@
 from django import forms
 from .models import Post, Comment
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title','content','draft','tags','image']
+        widgets = {
+            'content': SummernoteWidget(),
+            'bar': SummernoteInplaceWidget(),
+        }
         # exclude = ('author','Created date',)
 
 class CommentForm(forms.ModelForm):
